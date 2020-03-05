6aee46309a048690342f1a7ea2646055c1dc4662
Karen Garcia <kgarcia@i3screen.com>
2020-03-05 06:00:45 -0700

    working on day 11, got error with the forEach on skip buttons

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -6,5 +6,34 @@ const progressBar = playerControls.querySelector('.progress');
 const progressFilled = playerControls.querySelector('.progress_filled');
 
 const toggle =  playerControls.querySelector('.toggle');
-const skipButtons = playerControls.querySelector('[data-skip]');
+const skipButtons = player.querySelector('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+
+
+function togglePlay(){
+    if(video.paused){
+        video.play();
+    }else{
+        video.pause();
+    }
+
+}
+
+function upDateButton(){
+
+    const icon = this.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon;
+}
+
+function skip(){
+    console.log('skipping!');
+}
+
+video.addEventListener('click', togglePlay);
+toggle.addEventListener('click', togglePlay);
+video.addEventListener('play', upDateButton);
+video.addEventListener('pause', upDateButton);
+
+skipButtons.forEach(button=> button.addEventListener('click', skip));
+
+//stopped at 12 skipButtons for each not working giving error 
